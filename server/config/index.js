85ab7de96a0494a47731d3e07770caa0ea688c2e
89Ih <ehabhamza94@gmail.com>
2024-06-23 18:32:55 +0200

    P-URL

--- server/config/index.js
+++ server/config/index.js
@@ -4,12 +4,12 @@ const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const FRONTEND_URL = process.env.ORIGIN;
-
+const PRODUCTION_URL = process.env.PRODUCTION_URL;
 module.exports = (app) => {
   app.set("trust proxy", 1);
   app.use(
     cors({
-      origin: [FRONTEND_URL],
+      origin: [FRONTEND_URL,PRODUCTION_URL],
       methods:["POST","GET","DELETE","PUT"]
     })
   );
