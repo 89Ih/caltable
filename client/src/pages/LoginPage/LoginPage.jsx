7115136353acbd159d9f9642bfde3e2c90670d9e
89Ih <ehabhamza94@gmail.com>
2024-06-24 09:50:09 +0200

    test

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -10,13 +10,11 @@ function LoginPage() {
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
   const { storeToken, authenticateUser } = useContext(AuthContext);
-  const handleEmail = (e) => setEmail(e.target.value);
-  const handlePassword = (e) => setPassword(e.target.value);
+
   const handleLoginSubmit = (e) => {
     e.preventDefault();
-    const requestBody = { email, password };
     authService
-      .login(requestBody)
+      .login({ email, password })
       .then((response) => {
         storeToken(response.data.authToken);
         authenticateUser();
@@ -27,36 +25,34 @@ function LoginPage() {
         setErrorMessage(errorDescription);
       });
   };
-
   return (
-
     <div className="p-5 flex flex-col items-center gap-10">
       <h1 className="text-5xl">Login</h1>
-      <form 
-          onSubmit={handleLoginSubmit} 
-          className="flex flex-col justify-center items-center gap-5 _login">
-          <input
-            className="w-96 h-10 pl-2 rounded-lg border border-solid  bg-transparent" 
-          type="email" 
-          name="email" 
+      <form
+        onSubmit={handleLoginSubmit}
+        className="flex flex-col justify-center items-center gap-5 _login">
+        <input
+          className="w-96 h-10 pl-2 rounded-lg border border-solid  bg-transparent"
+          type="email"
+          name="email"
           placeholder="Email :"
-          value={email} 
-            onChange={handleEmail} />
-          <input
-             className="w-96 h-10 pl-2 rounded-lg border border-solid bg-transparent" 
-            placeholder="Password :"
-            type="password"
-            name="password"
-            value={password}
-            onChange={handlePassword}
-          />
-        <button 
-         className="w-96 h-10 pl-2 rounded-lg border border-solid bg-transparent"
-        type="submit">Login</button>
+          value={email}
+          onChange={(event) => setEmail(event.target.value)} />
+        <input
+          className="w-96 h-10 pl-2 rounded-lg border border-solid bg-transparent"
+          placeholder="Password :"
+          type="password"
+          name="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button
+          className="w-96 h-10 pl-2 rounded-lg border border-solid bg-transparent"
+          type="submit">Login</button>
       </form>
       {errorMessage && <p className="error-message">{errorMessage}</p>}
     </div>
- );
+  );
 }
 
 export default LoginPage;
--- server/config/index.js
+++ server/config/index.js
@@ -5,12 +5,15 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const FRONTEND_URL = process.env.ORIGIN;
 const PRODUCTION_URL = process.env.PRODUCTION_URL;
-module.exports = (app) => {
+const demoP ='http://localhost:54904';
+
+module.exports = app => {
   app.set("trust proxy", 1);
   app.use(
     cors({
-      origin: [FRONTEND_URL,PRODUCTION_URL],
-      methods:["POST","GET","DELETE","PUT"]
+      origin: [FRONTEND_URL,PRODUCTION_URL,demoP],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
     })
   );
   app.use(logger("dev"));
